8ea9736bb0beb12164b255ffe9b04664ed4c5254
Adebisi Abdussamad <adebisiabdussamadayomide@gmail.com>
2023-04-05 10:45:32 +0100

    working with Arrays and Objects

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-let greetingText = 'Hi my name is Bisi!';
-let userAge = 32;
-alert(greetingText);
-greetingText = 'Yoo, Whats up!!';
-alert(greetingText);
-alert(userAge);
+//arrays
+let hobbies = ['Sports', 'Cooking', 'Reading'];
+
+//objects
+let job = {
+  title: 'Developer',
+  places: 'Abuja',
+  salary: 50000
+};
+//Accessing the Arrays
+alert(hobbies[0]);
+alert(hobbies[1]);
+alert(hobbies[2]);
+//Accessing the Objects
+alert(job.title);
