bc9b0f34466a5e282525c4bce4f600068587d955
Adebisi Abdussamad <adebisiabdussamadayomide@gmail.com>
2023-04-06 16:51:47 +0100

    working with functions

--- app.js
+++ app.js
@@ -15,14 +15,28 @@ let job = {
   places: 'Abuja',
   salary: 50000
 };
+
 //Accessing the Arrays
 //alert(hobbies[0]);
 //alert(hobbies[1]);
 //alert(hobbies[2]);
+
 //Accessing the Objects
 //alert(job.title);
 
+//functions
+let adultYears;
+
+function calculateAdultYears() {
+  adultYears = userAge - 18;
+}
+
 //performing Operations
-userAge = 45;
-let adultYears = userAge - 18;
+calculateAdultYears();
 alert(adultYears);
+
+userAge = 45;
+calculateAdultYears();
+
+alert(adultYears);
+
