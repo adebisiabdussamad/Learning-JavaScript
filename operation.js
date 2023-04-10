9fb7627992776ead63fc95d6d0e2b4eb33c33202
Adebisi Abdussamad <adebisiabdussamadayomide@gmail.com>
2023-04-10 21:46:00 +0100

    working with maths shorthand

--- operation.js
+++ operation.js
@@ -9,3 +9,12 @@ console.log(23 / 3);
 
 console.log(23 % 3);
 console.log(23 % 4);
+
+// Maths rules
+
+let result = (10 + 3 - 5);
+result++; //result = result + 1; 
+result += 5; //result = result + 5; 
+result -= 5; //result = result - 5;
+
+console.log(result);
